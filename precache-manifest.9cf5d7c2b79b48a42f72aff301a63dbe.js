c4e0efc11546bd498cd02d993592ff14ea41a6f9
Jiahua Liao <ljh15088131916@gmail.com>
2020-06-06 20:57:35 -0400

    Updates

---
+++ precache-manifest.9cf5d7c2b79b48a42f72aff301a63dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b385f254a321a798597c8410dd46fb65",
+    "url": "/TTP-GIPHY-PROJECT/index.html"
+  },
+  {
+    "revision": "92dd431c90622ed3b3ab",
+    "url": "/TTP-GIPHY-PROJECT/static/css/main.194e1b33.chunk.css"
+  },
+  {
+    "revision": "0377d8cc95c4da26e05e",
+    "url": "/TTP-GIPHY-PROJECT/static/js/2.ce05f851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TTP-GIPHY-PROJECT/static/js/2.ce05f851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dd431c90622ed3b3ab",
+    "url": "/TTP-GIPHY-PROJECT/static/js/main.d508dc02.chunk.js"
+  },
+  {
+    "revision": "1d57d4fafce02004f137",
+    "url": "/TTP-GIPHY-PROJECT/static/js/runtime-main.2dcca7f5.js"
+  }
+]);
